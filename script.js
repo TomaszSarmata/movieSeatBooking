2838b70abbeed812e7e6a9784087f1d645463b38
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-23 15:08:24 +0000

    created local storage for our selected seats to persist the values across various reloads

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ let ticketPrice = +movieSelect.value;
 
 const updateSelectedCount = () => {
   let numberOfSeat = document.querySelectorAll(".row .seat.selected").length;
+
+  //copy selected seats into an array (for the local storage)
+  //map through an array
+  //return a new array indexes
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const seatsIndex = [...selectedSeats].map(
+    (seat) => [...seats].indexOf(seat) //so here we are mapping out all indexes of our seats that are not occupied and are returning the index of our selected seat/s that we are going to pass to our local storage
+  );
+
+  //setting up local storage
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   count.innerText = numberOfSeat;
   let totalPrice = ticketPrice * numberOfSeat;
   total.innerHTML = totalPrice;
@@ -20,7 +32,6 @@ movieSelect.addEventListener("change", (e) => {
 
 //seat click event
 container.addEventListener("click", function (e) {
-  console.log(ticketPrice);
   Array.from(seats, (seat) => {
     if (e.target === seat) {
       e.target.classList.toggle("selected");
