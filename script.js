bb1e661f97db21128d9482cb2e792f35fce465ff
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-11-23 11:29:14 +0000

    added an event listener to the parent container and used Array.from to loop over each seat and if the e.targer is a seat we add a class selected and change the value of the count

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 const ticketPrice = +movieSelect.value;
+
+container.addEventListener("click", function (e) {
+  Array.from(seats, (seat) => {
+    if (e.target === seat) {
+      e.target.classList.toggle("selected");
+      let numberOfSeat = document.querySelectorAll(
+        ".row .seat.selected"
+      ).length;
+      console.log(numberOfSeat);
+      count.innerText = numberOfSeat;
+    }
+  });
+});
+
+//let numberOfSeats = document.querySelectorAll(".row .seat.selected").length;
